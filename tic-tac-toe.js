49b2e7aa6ed945e24ecc91588a4ac141692fb160
Zoya Hanson <zoya.hanson@mymona.uwi.edu>
2024-10-27 21:57:17 -0400

    Add X and O functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,3 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
         square.classList.add('square');
     });
 });
+
+window.onload = function() {
+    const squares = document.querySelectorAll('#board div');
+    let currentPlayer = 'X';  
+
+    squares.forEach(square => {
+        square.addEventListener('click', function() {
+
+            if (square.textContent === '') {
+                square.textContent = currentPlayer;
+                square.classList.add(currentPlayer);
+
+                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            }
+        });
+    });
+};
